2a65294f842a1692d3b6f5ee8c207e18bdf074ae
usamathescientist <usamathescientist@gmail.com>
2020-01-13 02:10:53 +0500

    Commit 3

--- test.py
+++ test.py
@@ -2,16 +2,15 @@ import time
 from selenium import webdriver
 
 SCROLL_PAUSE_TIME = 3
-driver = webdriver.Chrome()
 
-my_url = 'https://www.ces.tech/Show-Floor/Exhibitor-Directory.aspx?searchTerm=&sortBy=alpha&filter=A&pageNo=1&pageSize=30'
+driver = webdriver.Chrome()
+my_url = 'https://www.ces.tech/Show-Floor/Exhibitor-Directory.aspx?pageSize=300'
 driver.get(my_url)
 
-
-# Get scroll height
-last_height = driver.execute_script("return document.body.scrollHeight")
-
 while True:
+    # Get page height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
     # Scroll down to bottom
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
@@ -20,22 +19,14 @@ while True:
 
     # Calculate new scroll height and compare with last scroll height
     new_height = driver.execute_script("return document.body.scrollHeight")
+
     if new_height == last_height:
         break
-    last_height = new_height
 
-x=1
+x = 1
 for item in driver.find_elements_by_class_name("listingCard"):
     print(item)
-    x=x+1
-print(x)
-# aa = driver.find_element_by_class_name("listingCard")
+    x = x + 1
 
-# driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-# driver.execute_script("window.scrollTo(0, Y)")
-# driver.get('file:///C:/Users/zaina/Downloads/zain/test1.html')
-# print(driver.page_source)
-# print(driver.get_log('browser'))
-# driver.execute_script("document.getElementById('intro-text').innerHTML = 'This is my script.'")
-# print(driver.page_source)
+print(x)
 driver.close()
